b1f992d3089a45554198e280eeeb13def6ae2d77
dabalime133 <dabalime133@gmail.com>
2026-04-21 16:56:26 +0400

    Create translations.ts

---
+++ src/components/translations.ts
@@ -0,0 +1,8 @@
+export const translations = {
+  RU: { play: "ИГРАТЬ", howTo: "Как играть", strategies: "Стратегии", win: "Выигрыш" },
+  EN: { play: "PLAY NOW", howTo: "How to Play", strategies: "Strategies", win: "Win" },
+  KZ: { play: "ОЙНАУ", howTo: "Қалай ойнау керек", strategies: "Стратегиялар", win: "Ұтыс" },
+  UZ: { play: "O'YNASH", howTo: "Qanday o'ynash", strategies: "Strategiyalar", win: "Yutuq" },
+  TR: { play: "OYNA", howTo: "Nasıl Oynanır", strategies: "Stratejiler", win: "Kazanç" }
+};
+export type TranslationKeys = typeof translations['RU'];
